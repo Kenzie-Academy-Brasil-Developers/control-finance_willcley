dbd6733faa9148f8fb663ac3dd22fbb2ab9215af
Willcley Melo <willcley2003@gmail.com>
2023-06-09 09:25:00 -0300

    add functions & style correction

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,111 @@
-/* Desenvolva sua lógica aqui */
+function sumValors(array) {
+    let sum = 0;
+    array.forEach((element) => sum += element.value);
+    return sum.toFixed(2);
+}
+
+function showValues(array) {
+    const totalValor = document.querySelector('.valorsSum');
+    const valorsSum = sumValors(array);
+
+    totalValor.innerText = `R$${valorsSum}`.replace('.', ',');
+}
+
+function renderInsertedValues(array) {
+    const allValorsList = document.querySelector('.app__allValors');
+    const allValorsEmpty = document.querySelector('.allValors__empty');
+
+    allValorsList.innerHTML = '';
+    allValorsList.appendChild(allValorsEmpty);
+
+    if (array.length === 0) {
+        allValorsEmpty.classList.remove('close');
+        allValorsEmpty.classList.add('show');
+    } else {
+        allValorsEmpty.classList.remove('show');
+        allValorsEmpty.classList.add('close');
+        
+        array.forEach((element) => {
+            const item = document.createElement('li');
+            const value = document.createElement('p');
+            const itemButtons = document.createElement('div');
+            const buttonType = document.createElement('button');
+            const buttonTrash = document.createElement('button');
+    
+            item.classList.add('allValors__item');
+            value.classList.add('text-1');
+            itemButtons.classList.add('item__buttons');
+            buttonType.classList.add('btn--greylow');
+            buttonTrash.classList.add('btn--trash');
+    
+            value.innerText = `R$${element.value.toFixed(2)}`.replace('.', ',');
+            buttonType.innerText = valuesCategory[element.categoryID];
+
+            buttonTrash.addEventListener('click', () => {
+                const thisElementIndex = insertedValues.findIndex((value) => value === element);
+
+                insertedValues.splice(thisElementIndex, 1);
+
+                renderInsertedValues(insertedValues);
+                filterValues(insertedValuesFiltered);
+            });
+    
+            itemButtons.append(buttonType, buttonTrash);
+            item.append(value, itemButtons);
+            allValorsList.appendChild(item);
+        });
+    }
+    
+    showValues(array);
+}
+
+function filterValues(array) {
+    const navButtons = document.querySelector('.navbar__items').querySelectorAll('button');
+    
+    array = insertedValues;
+    
+    navButtons[0].addEventListener('click', () => {
+        array = insertedValues;
+        showValues(array);
+        renderInsertedValues(array);
+
+        navButtons.forEach((button) => {
+            button.classList.remove('btn--outline--active');
+            button.classList.add('btn--outline');
+        });
+
+        navButtons[0].classList.remove('btn--outline');
+        navButtons[0].classList.add('btn--outline--active');
+    });
+
+    navButtons[1].addEventListener('click', () => {
+        array = insertedValues.filter((element) => element.categoryID === 0);
+        showValues(array);
+        renderInsertedValues(array);
+
+        navButtons.forEach((button) => {
+            button.classList.remove('btn--outline--active');
+            button.classList.add('btn--outline');
+        });
+
+        navButtons[1].classList.remove('btn--outline');
+        navButtons[1].classList.add('btn--outline--active');
+    });
+
+    navButtons[2].addEventListener('click', () => {
+        array = insertedValues.filter((element) => element.categoryID === 1);
+        showValues(array);
+        renderInsertedValues(array);
+
+        navButtons.forEach((button) => {
+            button.classList.remove('btn--outline--active');
+            button.classList.add('btn--outline');
+        });
+
+        navButtons[2].classList.remove('btn--outline');
+        navButtons[2].classList.add('btn--outline--active');
+    });
+}
+
+renderInsertedValues(insertedValues);
+filterValues(insertedValuesFiltered);
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,84 @@
-/* Desenvolva sua lógica aqui */
+function showModal() {
+    const newValueButtons = document.querySelectorAll('.registerNewValue');
+    const modalController = document.querySelector('.modal__controller');
+
+    newValueButtons.forEach((element) => {
+        element.addEventListener('click', () => {
+            modalController.showModal();
+            modalController.style.top = '0';
+        })
+    })
+}
+
+function closeModal(category, categoryButtons) {
+    const modalController = document.querySelector('.modal__controller');
+    const inputModal = document.querySelector('.input__insertValue');
+    const closeModalButtons = document.querySelectorAll('.btn--closeModal');
+
+    closeModalButtons.forEach((element) => {
+        element.addEventListener('click', () => {
+            modalController.close();
+            modalController.style.top = '-150%';
+        });
+    });
+
+    closeModalButtons[1].addEventListener('click', () => {
+        inputModal.value = '';
+
+        category = undefined;
+
+        categoryButtons.forEach((button) => {
+            button.classList.remove('btn--outline--active');
+            button.classList.add('btn--outline');
+        });
+    });
+}
+
+function addNewValue() {
+    const modalController = document.querySelector('.modal__controller');
+    const inputModal = document.querySelector('.input__insertValue');
+    const categoryTypes = document.querySelectorAll('.categoryOption');
+    const buttonConfirm = document.querySelector('.btn--confirmModal');
+    let category;
+
+    categoryTypes.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            category = index;
+
+            categoryTypes.forEach((buttonToo) => {
+                buttonToo.classList.remove('btn--outline--active');
+                buttonToo.classList.add('btn--outline');
+            });
+
+            button.classList.remove('btn--outline');
+            button.classList.add('btn--outline--active');
+        });
+    });
+
+    buttonConfirm.addEventListener('click', () => {
+        if (inputModal.value == '' || category == undefined) {
+            alert('Ainda há campos não preenchidos...');
+        } else {
+            const newObj = {
+                id: insertedValues.length + 1,
+                value: parseFloat(inputModal.value),
+                categoryID: category
+            }
+
+            inputModal.value = '';
+            category = undefined;
+    
+            insertedValues.push(newObj);
+    
+            filterValues(insertedValuesFiltered);
+            renderInsertedValues(insertedValues);
+    
+            modalController.close();
+        }
+    });
+
+    closeModal(category, categoryTypes);
+}
+
+showModal();
+addNewValue();
